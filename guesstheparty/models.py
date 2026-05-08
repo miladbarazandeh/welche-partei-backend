8ad6093d08904fc8ae1547ea72c8b236429352e7
Milad Barazandeh <miladbeznd@gmail.com>
2026-05-08 11:24:52 +0900

    Add migration to alter image_url field in Politician model

--- guesstheparty/models.py
+++ guesstheparty/models.py
@@ -15,7 +15,7 @@ class Politician(models.Model):
     name = models.CharField(max_length=200)
     party = models.CharField(max_length=50, choices=CANONICAL_PARTIES)
     parliament = models.CharField(max_length=100)
-    image_url = models.URLField(blank=True)
+    image_url = models.URLField(max_length=500, blank=True)
     image_local = models.CharField(max_length=500, blank=True)
 
     def __str__(self):
